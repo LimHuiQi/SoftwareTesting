4840c1b3c75d84bdd6d61308b14de22623ae804d
LimHuiQi 287499 <limhuiqi96@gmail.com>
2024-01-13 03:41:27 +0800

    Update

---
+++ src/main/java/Group9_Lim/FIMS01_01.java
@@ -0,0 +1,134 @@
+package Group9_Lim;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.After;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class FIMS01_01 {
+    static WebDriver driver;
+
+    @Before
+    public void beforeTest() throws InterruptedException {
+        // Set up WebDriver for Edge browser
+        System.setProperty("webdriver.edge.driver","C:\\Users\\limhu\\OneDrive\\Desktop\\UUM\\Sem 5\\Software Testing & Quality Accurance\\EdgeDriver\\msedgedriver.exe" );
+        driver = new EdgeDriver();
+        driver.manage().window().maximize();
+
+        // Open the FIMS application
+        driver.get("https://fimsclone.kerisi.my/");
+        Thread.sleep(3000);
+
+        // Input Email
+        driver.findElement(By.xpath("//*[@id=\"userID\"]")).sendKeys("ENTRY4");
+        Thread.sleep(1000);
+
+        // Input Password
+        driver.findElement(By.xpath("//*[@id=\"userPassword\"]")).sendKeys("qwertyuiop");
+        Thread.sleep(1000);
+
+        // Click Login
+        driver.findElement(By.xpath("//*[@id=\"login\"]")).click();
+        Thread.sleep(1000);
+
+        // Select Side Menu
+        driver.findElement(By.xpath("//*[@id=\"sideMenuLeft\"]/div[2]")).click();
+        Thread.sleep(1000);
+
+        // Select Portal
+        driver.findElement(By.xpath("//*[@id=\"menu_id_1533\"]")).click();
+        Thread.sleep(1000);
+
+        // Select Stock Application
+        driver.findElement(By.xpath("//*[@id=\"menu_id_2867\"]")).click();
+        Thread.sleep(1000);
+
+        // Select New Application
+        driver.findElement(By.xpath("//*[@id=\"menu_id_2868\"]")).click();
+        Thread.sleep(1000);
+
+        // Assert to verify if the user is logged in successfully
+        Assert.assertEquals("User logged in successfully? ", "Portal / Stock Application / New Application", driver.getTitle().trim());
+        System.out.println("User logged in successfully.");
+    }
+
+    @Test
+    public void FIMS01_01_01_searchStoreName() throws InterruptedException {
+        // Select Store Name
+        driver.findElement(By.xpath("//*[@id=\"inputArea_sma_store_code\"]/span/span[2]/span")).click();
+        Thread.sleep(1000);
+
+        // Input Store Name
+        driver.findElement(By.xpath("/html/body/span/span/span[1]/input")).sendKeys("UUMIT");
+        Thread.sleep(1000);
+
+        // Select Store
+        driver.findElement(By.xpath("//*[@id=\"select2-sma_store_code-results\"]/li/table/tbody/tr/td[3]")).click();
+        Thread.sleep(1000);
+
+        // Get Text From Page
+        String actualStoreName = driver.findElement(By.xpath("//*[@id=\"select2-sma_store_code-container\"]")).getText();
+        String expectedStoreName = "UUMIT";
+
+        // Assert to verify if the actual store name contains the expected store name
+        Assert.assertTrue("Actual store name contains expected store name", actualStoreName.contains(expectedStoreName));
+        System.out.println("User successfully handled the scenario with valid data input.");
+    }
+
+    @Test
+    public void FIMS01_01_02_searchStoreNameNoData() throws InterruptedException {
+        // Select Store Name
+        driver.findElement(By.xpath("//*[@id=\"inputArea_sma_store_code\"]/span/span[2]/span")).click();
+        Thread.sleep(1000);
+
+        // Input Empty Store Name (No Data)
+        driver.findElement(By.xpath("/html/body/span/span/span[1]/input")).sendKeys("");
+        Thread.sleep(1000);
+
+        // Get Text From Page
+        String actualStoreName = driver.findElement(By.xpath("//*[@id=\"select2-sma_store_code-container\"]")).getText();
+
+        // Check for the presence of "Masukkan lagi 1 atau lebih huruf" message
+        WebElement enterDataMessage = driver.findElement(By.xpath("//*[@id=\"select2-sma_store_code-results\"]/li"));
+        System.out.println("Message displayed: " + enterDataMessage.getText());
+
+        // Assert to verify if "Masukkan lagi 1 atau lebih huruf" message is displayed for no data
+        Assert.assertTrue("Masukkan lagi 1 atau lebih huruf message displayed for no data input", enterDataMessage.isDisplayed());
+        System.out.println("User successfully handled the scenario with no data.");
+    }
+
+    @Test
+    public void FIMS01_01_03_searchStoreNameInvalidData() throws InterruptedException {
+        // Select Store Name
+        driver.findElement(By.xpath("//*[@id=\"inputArea_sma_store_code\"]/span/span[2]/span")).click();
+        Thread.sleep(1000);
+
+        // Input Invalid Store Name
+        String invalidStoreName = "InvalidStore123";
+        driver.findElement(By.xpath("/html/body/span/span/span[1]/input")).sendKeys(invalidStoreName);
+        Thread.sleep(1000);
+
+        // Check for the presence of "Tiada Data" message
+        WebElement tiadaDataMessage = driver.findElement(By.xpath("//*[@id=\"select2-sma_store_code-results\"]/li"));
+        System.out.println("Message displayed: " + tiadaDataMessage.getText());
+
+        // Assert to verify if "Tiada Data" message is displayed for invalid data
+        Assert.assertTrue("Tiada Data message displayed for invalid data", tiadaDataMessage.isDisplayed());
+        System.out.println("User successfully handled the scenario with invalid data input.");
+    }
+
+
+    @After
+    public void afterTest() throws InterruptedException {
+
+        // Stay on the page for 3 seconds
+        Thread.sleep(3000);
+
+        // Quit the driver
+        driver.quit();
+    }
+}
---
+++ src/main/java/Group9_Lim/FIMS01_04.java
@@ -0,0 +1,133 @@
+package Group9_Lim;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.After;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class FIMS01_04 {
+    static WebDriver driver;
+
+    @Before
+    public void beforeTest() throws InterruptedException {
+        // Set up WebDriver for Edge browser
+        System.setProperty("webdriver.edge.driver", "C:\\Users\\limhu\\OneDrive\\Desktop\\UUM\\Sem 5\\Software Testing & Quality Accurance\\EdgeDriver\\msedgedriver.exe");
+        driver = new EdgeDriver();
+        driver.manage().window().maximize();
+
+        // Open the FIMS application
+        driver.get("https://fimsclone.kerisi.my/");
+        Thread.sleep(3000);
+
+        // Print title and URL for debugging
+        String title = driver.getTitle();
+        String url = driver.getCurrentUrl();
+        System.out.println("Title: " + title);
+        System.out.println("URL: " + url);
+
+        // Input Email
+        driver.findElement(By.xpath("//*[@id=\"userID\"]")).sendKeys("ENTRY4");
+        Thread.sleep(1000);
+
+        // Input Password
+        driver.findElement(By.xpath("//*[@id=\"userPassword\"]")).sendKeys("qwertyuiop");
+        Thread.sleep(1000);
+
+        // Click Login
+        driver.findElement(By.xpath("//*[@id=\"login\"]")).click();
+        Thread.sleep(1000);
+
+        // Select Side Menu
+        driver.findElement(By.xpath("//*[@id=\"sideMenuLeft\"]/div[2]")).click();
+        Thread.sleep(1000);
+
+        // Select Portal
+        driver.findElement(By.xpath("//*[@id=\"menu_id_1533\"]")).click();
+        Thread.sleep(1000);
+
+        // Select Stock Application
+        driver.findElement(By.xpath("//*[@id=\"menu_id_2867\"]")).click();
+        Thread.sleep(1000);
+
+        // Select New Application
+        driver.findElement(By.xpath("//*[@id=\"menu_id_2868\"]")).click();
+        Thread.sleep(1000);
+
+        // Assert to verify if the user is logged in successfully
+        Assert.assertEquals("User logged in successfully? ", "Portal / Stock Application / New Application", driver.getTitle().trim());
+        System.out.println("User logged in successfully.");
+
+        // Select Store Name
+        driver.findElement(By.xpath("//*[@id=\"inputArea_sma_store_code\"]/span/span[2]/span")).click();
+        Thread.sleep(1000);
+
+        // Input Store Name
+        driver.findElement(By.xpath("/html/body/span/span/span[1]/input")).sendKeys("UUMIT");
+        Thread.sleep(1000);
+
+        // Select Store
+        driver.findElement(By.xpath("//*[@id=\"select2-sma_store_code-results\"]/li/table/tbody/tr/td[3]")).click();
+        Thread.sleep(1000);
+
+        // Search Store
+        driver.findElement(By.xpath("//*[@id=\"searchbtn\"]")).click();
+        Thread.sleep(1000);
+
+        // Get Text From Page
+        String actualTitle = driver.findElement(By.xpath("//*[@id=\"select2-sma_store_code-container\"]")).getText();
+        String expectedTitle = "UUMIT";
+
+        // Assert to verify if the actual title contains the expected title
+        Assert.assertTrue("Actual title contains expected title", actualTitle.contains(expectedTitle));
+        System.out.println("User search store successfully at requisition.");
+    }
+
+    @Test
+    public void TC01_02_01_addItem() throws InterruptedException {
+
+        // Select Add Item
+        driver.findElement(By.xpath("//*[@id=\"btn_add\"]/i")).click();
+        Thread.sleep(1000);
+
+        // Select Item Input Box
+        driver.findElement(By.xpath("//*[@id=\"inputArea_sit_store_item_id\"]/span/span[2]/span")).click();
+        Thread.sleep(1000);
+
+        // Select Item
+        driver.findElement(By.xpath("//*[@id=\"sit_store_item_id\"]/option[2]")).click();
+        Thread.sleep(1000);
+
+        // Select Item Input Box
+        driver.findElement(By.xpath("//*[@id=\"inputArea_sit_store_item_id\"]/span/span[2]/span")).click();
+        Thread.sleep(1000);
+
+        // Input Quantity Request
+        driver.findElement(By.xpath("//*[@id=\"srd_qty_request\"]")).sendKeys("10");
+        Thread.sleep(1000);
+
+        // Save Item
+        driver.findElement(By.xpath("//*[@id=\"srd_store_req_detl_id\"]")).click();
+        Thread.sleep(1000);
+
+        // Select ok for confirmation
+        driver.findElement(By.xpath("//*[@id=\"modalConfirm7\"]/div/div/div[3]/button[2]")).click();
+        Thread.sleep(1000);
+
+        // Assert to verify if the item is added successfully
+        Assert.assertTrue("Item added successfully.", true);
+        System.out.println("User successfully add an item.");
+    }
+
+    @After
+    public void afterTest() throws InterruptedException {
+
+        // Stay on the page for 3 seconds
+        Thread.sleep(3000);
+
+        // Quit the driver
+        driver.quit();
+    }
+}
